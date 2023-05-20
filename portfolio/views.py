dc989fdc9ae685036c2aec7c2fb361e9f92fa5ea
4Moyede <krkdgud9@gmail.com>
2023-05-20 19:30:27 +0900

    feat:Server로 부터 받은 데이터 화면 출력

--- portfolio/views.py
+++ portfolio/views.py
@@ -1,3 +1,91 @@
 from django.shortcuts import render
 
-# Create your views here.
+def getPortfolioIntroduce(request):
+    template = 'portfolio/index.html'
+    context = { 
+        "introduce": getIntroduce(),
+        "about" : getAbout(),
+        "study" : getStudy(),
+        "exprience" : getExperience(),
+        "skills" : getSkills(),
+        "services" : getService(),
+    }
+
+    return render(request, template, context)
+
+def getIntroduce():
+    introduce = {
+        "name": "강형구",
+        "jobGrp": "Software Engineer",
+        "aboutMe" : ""
+    }
+
+    return introduce
+
+def getAbout():
+    about = [
+        {
+            "key" : "From",
+            "val" : "Seoul, South Korea"
+        },
+        {
+            "key" : "Age",
+            "val" : "31"
+        },
+        {
+            "key" : "Gender",
+            "val" : "Male"
+        },
+    ]
+
+    return about
+
+def getStudy():
+    study = [
+        {
+            "date" : "23.09",
+            "title" : "SQLD",
+            "subTitle" : "Lisence",
+            "content" : "Kdata 데이터자격검정, SQL Developer"
+        },
+        {
+            "date" : "14.03 ~ 21.02",
+            "title" : "경희대학교 졸업",
+            "subTitle" : "University",
+            "content" : "소프트웨어융합대학 컴퓨터공학과 학사 졸업"
+        },
+    ]
+
+    return study
+
+def getExperience():
+    experience = [
+        {
+            "date" : "21.11 ~ Current",
+            "title" : "차세대 UCube 프로젝트",
+            "subTitle" : "LG CNS",
+            "content" : "LG U+의 차세대 UCube 상담파트 설계 및 개발 참여 \nFE(WebSqaure), BE(Spring Boot, MariaDB), MSA, Rest API, Kafka, MySQL"
+        },
+    ]
+
+    return experience
+
+def getSkills():
+    skills = [
+        {
+            "name" : "java",
+            "proficiency" : "80%"
+        }
+    ]
+
+    return skills
+
+def getService():
+    services = [
+        {
+            "name" : "WebApp",
+            "comment" : "Something"
+        }
+    ]
+
+    return services
